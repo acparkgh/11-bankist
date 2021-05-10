5277d4ef308740cb20f02675f0953cf22b413326
acparkgh <acpark123@gmail.com>
2021-05-10 00:34:59 -0400

    first commit

---
+++ starter/script.js
@@ -0,0 +1,152 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+// let arr = ["a", "b", "c", "d", "e", "f", "g"]
+
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 5));
+// console.log(arr.slice(-4));
+// console.log(arr.slice(1, -4));
+// const arr1 = arr.slice();
+// const arr2 = [...arr];
+// console.log(arr);
+// console.log(arr1);
+// console.log(arr2);
+
+// // splice mutate original array:
+
+// let arr10 = ["a", "b", "c", "d", "e", "f", "g"];
+// const spliceReturns1 = arr10.splice(3);
+// console.log(spliceReturns1);
+// console.log(arr10);
+// const pop1 = arr10.pop();
+// console.log(pop1);
+// console.log(arr10);
+
+// console.log(arr10.concat(arr1));
+// const allArrays = [...arr10, ...arr1];
+// console.log(allArrays.join(" - "));
+// console.log(arr10.includes("x"))
+// console.log(arr10.includes("b"))
+
+// for (const eachItem of arr1.entries()) {
+//   console.log(eachItem);
+// }
+
+
+// for (const movement of movements.entries()) {
+// for (const [index, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${index + 1}: You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`)
+//   }
+// }
+
+// console.log("-------------------------------");
+
+// const moveMoney = (money, index, array) => {
+//   if (money > 0) {
+//     console.log(`Movement ${index + 1}: You deposited ${money}`);
+//   } else {
+//     console.log(`Movement ${index + 1}: You withdrew ${Math.abs(money)}`)
+//   }
+//   console.log(array);
+// }
+
+// movements.forEach(moveMoney);
+
+// movements.forEach((money) => {
+//   if (money > 0) {
+//     console.log(`You deposited ${money}`);
+//   } else {
+//     console.log(`You withdrew ${Math.abs(money)}`)
+//   }
+// });
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+  // console.log(map);
+})
+
+const currenciesUnique = new Set(["USD", "BGP", "USD", "EUR", "EUR"]);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach((value, key, set) => {
+  console.log(`${key}: ${value}`);
+  // console.log(set);
+});
---
+++ starter/scriptCodeChallenge.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const checkDogs = function (dogsJulia, dogsKate) {
+  
+  const dogsOnlyJulia = dogsJulia.slice(1, -2);
+  // const allDogs = dogsOnlyJulia.concat(dogsKate);
+  const allDogs = [...dogsOnlyJulia, ...dogsKate];
+    // console.log(allDogs);
+
+  const adultOrPuppy = function (dog, i) {
+    const message = (
+      dog >= 3 ?
+        `Dog number ${i + 1} is an adult, and is ${dog} years old`
+         :
+        `Dog number ${i + 1} is still a puppy 🐶`
+    )
+    
+    console.log(message);
+  }
+
+  allDogs.forEach(adultOrPuppy)
+
+}
+
+const juliasData = [3, 5, 2, 12, 7];
+const katesData = [4, 1, 15, 8, 3];
+
+// const juliasData = [9, 16, 6, 8, 3];
+// const katesData = [10, 5, 6, 1, 4];
+
+checkDogs(juliasData, katesData);
+
+
+const calcAverageHumanAge = function (dogAgeArray) {
+  const humanAges = dogAgeArray.map((dog) => {
+    return dog <= 2 ? 2 * dog : 16 + dog * 4;
+  });
+
+  const olderThanEighteen = humanAges.filter((humanAge) => {
+    return humanAge >= 18
+  });
+  
+  const aveHumanAge = olderThanEighteen.reduce((acc, humanAge) => {
+    return acc + humanAge;
+  }, 0) / olderThanEighteen.length;
+    
+  console.log(humanAges);
+  console.log(olderThanEighteen);
+  console.log(aveHumanAge);
+};
+
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
+
+calcAverageHumanAge(data1);
+calcAverageHumanAge(data2);
